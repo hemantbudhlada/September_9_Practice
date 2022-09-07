52709701fa565961b0de0e2622e67751362b29d9
hanumant <hemantguys@gmail.com>
2022-09-07 16:45:47 +0530

    Version

---
+++ Mytodayprojectdemo/src/sampleproject/class2.java
@@ -0,0 +1,13 @@
+package sampleproject;
+
+public class class2 
+{
+	public void method1()
+	{
+		System.out.println("This is my first mehtod");
+		System.out.println("This is my first mehtod");
+		System.out.println("This is my first mehtod");
+		System.out.println("This is my first mehtod");
+	}
+
+}
