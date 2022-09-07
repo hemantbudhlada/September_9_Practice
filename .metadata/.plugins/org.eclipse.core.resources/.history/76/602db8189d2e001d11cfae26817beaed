0de76fa861e66e497ab070f00bec781303c7b072
hanumant <hemantguys@gmail.com>
2022-09-07 16:36:53 +0530

    changes

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/76/602db8189d2e001d11cfae26817beaed
@@ -0,0 +1,10 @@
+package sampleproject;
+
+public class class1 
+{
+	public void method1()
+	{
+		System.out.println("This is my first mehtod");
+	}
+
+}
--- Mytodayprojectdemo/src/sampleproject/class1.java
+++ Mytodayprojectdemo/src/sampleproject/class1.java
@@ -5,6 +5,9 @@ public class class1
 	public void method1()
 	{
 		System.out.println("This is my first mehtod");
+		System.out.println("This is my first mehtod");
+		System.out.println("This is my first mehtod");
+		System.out.println("This is my first mehtod");
 	}
 
 }
